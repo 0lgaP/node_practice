b4e3d91f3a87b27dbb01e0c9368c8dd9f2ef8e6e
0lgatron <perronolga@gmail.com>
2022-05-23 14:47:45 -0400

    export example

---
+++ app.js
@@ -0,0 +1,14 @@
+// const counter = require("./mymodule");
+
+// counter.inc()
+// counter.inc()
+// counter.inc()
+
+// console.log(counter.getCount())
+
+const {inc, dec, getCount} = require("./mymodule");
+inc()
+inc()
+dec()
+
+console.log(getCount())
