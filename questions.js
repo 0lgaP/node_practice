d371548008dccb45c6b5d81c2e25eb73b4832edf
0lgatron <perronolga@gmail.com>
2022-05-23 16:26:51 -0400

    custom events, EventEmitter

--- questions.js
+++ questions.js
@@ -1,13 +1,15 @@
 const collectAnswers = require("./lib/collectAnswers.js");
 const questions = [
-  "What’s your favorite way to spend a day off?",
+  "Whats your favorite way to spend a day off?",
   "What was your favorite age growing up?",
   "What's your favorite ice cream topping?",
 ];
 
 //function call: collect answers
-collectAnswers(questions, (answers) => {
+const answerEvent = collectAnswers(questions, (answers) => {
   console.log(`Thank you for your answers`);
-  console.log(answers);
+  console.log(` ${answers}`);
   process.exit();
 });
+
+answerEvent.on("answer", answer => console.log(`Her answered: ${answer}`))
